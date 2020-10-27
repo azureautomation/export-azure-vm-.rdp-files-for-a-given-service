638402fdc008caa2d368cacd26986f03a137b0d2
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 15:31:32 +0530

    initial commit

---
+++ BulkExportAzureVM-RDPfiles.ps1
@@ -0,0 +1,44 @@
+﻿<#
+.SYNOPSIS 
+    Export VM .RDP files for a given Azure service.
+
+.DESCRIPTION
+    The following script will export all VM .RDP connections to c:\AzureRDP folder.  The folder must be created by hand before the script is run.
+
+.PARAMETER ServiceName
+    Cloud Service the Azure VM is running under.
+
+.PARAMETER SubscriptionName
+    Name of the Azure subscription being selected.
+
+#>
+
+
+Param
+
+(
+
+[parameter(Mandatory=$true)]
+
+[String]
+
+$ServiceName,
+
+[parameter(Mandatory=$true)]
+
+[String]
+
+$SubscriptionName
+
+
+)
+
+Add-AzureAccount
+
+select-azuresubscription "$SubscriptionName"
+
+Get-AzureVM $ServiceName | foreach { 
+	$rdpexportfile = 'c:\AzureRDP\' + $_.Name + '.rdp'
+	Get-AzureRemoteDesktopFile -ServiceName $ServiceName -Name $_.Name -LocalPath $rdpexportfile
+}
+
